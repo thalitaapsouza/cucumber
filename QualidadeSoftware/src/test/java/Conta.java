72d48740ecd78767dfc8b3bd8829e5d8fa3fadc4
thalitaapsouza <110432350+thalitaapsouza@users.noreply.github.com>
2022-11-03 18:28:00 -0300

    comentarios código

--- QualidadeSoftware/src/test/java/Conta.java
+++ QualidadeSoftware/src/test/java/Conta.java
@@ -4,29 +4,64 @@ import io.cucumber.java.en.When;
 
 public class Conta {
 	
+	/**
+	 * 
+	 * @author thaap
+	 * @param valor - Parametro do tipo inteiro dado com o nome de valor.
+	 * 
+	 */
+	
 	@Given("um cliente especial com saldo atual de {int} reais")
-	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
+	public void um_cliente_especial_com_saldo_atual_de_reais(Integer valor) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * 
+	 * @author thaap
+	 * @param valor - Parametro do tipo inteiro dado com o nome de valor.
+	 * 
+	 */
 
 	@When("for solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer valor) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * 
+	 * @author thaap
+	 * @param valor - Parametro do tipo inteiro dado com o nome de valor.
+	 * 
+	 */
 
 	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
-	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer valor) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * 
+	 * @author thaap
+	 * @param valor - Parametro do tipo inteiro dado com o nome de valor.
+	 * 
+	 */
 
 	@When("solicitar um saque de {int} reais")
-	public void solicitar_um_saque_de_reais(Integer int1) {
+	public void solicitar_um_saque_de_reais(Integer valor) {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new io.cucumber.java.PendingException();
 	}
+	
+	/**
+	 * 
+	 * @author thaap
+	 * @param - Não passa nenhum tipo de parametro no metodo.
+	 * 
+	 */
 
 	@Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
 	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
