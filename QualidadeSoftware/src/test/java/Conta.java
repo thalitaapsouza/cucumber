47e1348560067919b8c3c202e9a8b1a94a1fb591
thalitaapsouza <110432350+thalitaapsouza@users.noreply.github.com>
2022-11-03 18:02:00 -0300

    primeiro commit

---
+++ QualidadeSoftware/src/test/java/Conta.java
@@ -0,0 +1,36 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Conta {
+	
+	@Given("um cliente especial com saldo atual de {int} reais")
+	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("for solicitado um saque no valor de {int} reais")
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("solicitar um saque de {int} reais")
+	public void solicitar_um_saque_de_reais(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
+	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
